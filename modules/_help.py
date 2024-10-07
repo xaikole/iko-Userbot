e58f72a2b90500f4606db39c8a192d81d0b015eb
Xaikole <ozosole4@gmail.com>
2024-10-07 23:11:57 +0700

    Update _help.py

--- modules/_help.py
+++ modules/_help.py
@@ -17,30 +17,54 @@ from . import HNDLR, LOGS, asst, ayra_cmd, get_string
 _main_help_menu = [
     [
         Button.inline(get_string("help_4"), data="uh_Official_"),
+        Button.inline(get_string("help_5"), data="uh_Addons_"),
     ],
+    [
+        Button.inline(get_string("help_6"), data="uh_VCBot_"),
+        Button.inline(get_string("help_7"), data="inlone"),
+    ],
+    [
+        Button.inline(get_string("help_8"), data="ownr"),
+        Button.url(
+            get_string("help_9"), url=f"https://t.me/{asst.me.username}?start=set"
+        ),
+    ],
+    [Button.inline(get_string("help_10"), data="close")],
 ]
 
 
-@ayra_cmd(pattern="[hH][eE][lL][pP]( (.*)|$)")
-async def _help(ayra):
-    plug = ayra.pattern_match.group(1).strip()
-    chat = await ayra.get_chat()
+@ultroid_cmd(pattern="help( (.*)|$)")
+async def _help(ult):
+    plug = ult.pattern_match.group(1).strip()
+    chat = await ult.get_chat()
     if plug:
         try:
             if plug in HELP["Official"]:
                 output = f"**Plugin** - `{plug}`\n"
                 for i in HELP["Official"][plug]:
                     output += i
-                output += "\n© @Darensupport"
-                await ayra.eor(output)
+                output += "\n© @TeamUltroid"
+                await ult.eor(output)
+            elif HELP.get("Addons") and plug in HELP["Addons"]:
+                output = f"**Plugin** - `{plug}`\n"
+                for i in HELP["Addons"][plug]:
+                    output += i
+                output += "\n© @TeamUltroid"
+                await ult.eor(output)
+            elif HELP.get("VCBot") and plug in HELP["VCBot"]:
+                output = f"**Plugin** - `{plug}`\n"
+                for i in HELP["VCBot"][plug]:
+                    output += i
+                output += "\n© @TeamUltroid"
+                await ult.eor(output)
             else:
                 try:
                     x = get_string("help_11").format(plug)
                     for d in LIST[plug]:
                         x += HNDLR + d
                         x += "\n"
-                    x += "\n© @Darensupport"
-                    await ayra.eor(x)
+                    x += "\n© @TeamUltroid"
+                    await ult.eor(x)
                 except BaseException:
                     file = None
                     compare_strings = []
@@ -56,46 +80,54 @@ async def _help(ayra):
                     if not file:
                         # the enter command/plugin name is not found
                         text = f"`{plug}` is not a valid plugin!"
-                        if best_match := next(
-                            (
-                                _
-                                for _ in compare_strings
-                                if plug in _ and not _.startswith("_")
-                            ),
-                            None,
-                        ):
+                        best_match = None
+                        for _ in compare_strings:
+                            if plug in _ and not _.startswith("_"):
+                                best_match = _
+                                break
+                        if best_match:
                             text += f"\nDid you mean `{best_match}`?"
-                        return await ayra.eor(text)
-                    output = f"**Perintah** `{plug}` **ditemukan dalam** - `{file}`\n"
+                        return await ult.eor(text)
+                    output = f"**Command** `{plug}` **found in plugin** - `{file}`\n"
                     if file in HELP["Official"]:
                         for i in HELP["Official"][file]:
                             output += i
-                    output += "\n© @Darensupport"
-                    await ayra.eor(output)
+                    elif HELP.get("Addons") and file in HELP["Addons"]:
+                        for i in HELP["Addons"][file]:
+                            output += i
+                    elif HELP.get("VCBot") and file in HELP["VCBot"]:
+                        for i in HELP["VCBot"][file]:
+                            output += i
+                    output += "\n© @TeamUltroid"
+                    await ult.eor(output)
         except BaseException as er:
             LOGS.exception(er)
-            await ayra.eor("Error 🤔 occured.")
+            await ult.eor("Error 🤔 occured.")
     else:
         try:
-            results = await ayra.client.inline_query(asst.me.username, "ayra")
+            results = await ult.client.inline_query(asst.me.username, "ultd")
         except BotMethodInvalidError:
             z = []
             for x in LIST.values():
                 z.extend(x)
             cmd = len(z) + 10
-            return await ayra.reply(
+            if udB.get_key("MANAGER") and udB.get_key("DUAL_HNDLR") == "/":
+                _main_help_menu[2:3] = [[Button.inline("• Manager Help •", "mngbtn")]]
+            return await ult.reply(
                 get_string("inline_4").format(
                     OWNER_NAME,
                     len(HELP["Official"]),
+                    len(HELP["Addons"] if "Addons" in HELP else []),
                     cmd,
                 ),
+                file=inline_pic(),
                 buttons=_main_help_menu,
             )
         except BotResponseTimeoutError:
-            return await ayra.eor(
+            return await ult.eor(
                 get_string("help_2").format(HNDLR),
             )
         except BotInlineDisabledError:
-            return await ayra.eor(get_string("help_3"))
-        await results[0].click(chat.id, reply_to=ayra.reply_to_msg_id, hide_via=True)
-        await ayra.delete()
+            return await ult.eor(get_string("help_3"))
+        await results[0].click(chat.id, reply_to=ult.reply_to_msg_id, hide_via=True)
+        await ult.delete()
